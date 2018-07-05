cbd87fc3af2ac8b13fc444d7c2b738c4699d1d75
Josh <JTCheung3@gmail.com>
2018-07-05 02:40:22 -0700

    update to sidebar

--- src/Queue.js
+++ src/Queue.js
@@ -95,6 +95,223 @@ class Queue extends Component {
           <td className="listings"> Jose </td>
           <td className="listings"> Jose </td>
         </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 15 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 16 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 17 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 18 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 19 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 20 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 21 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 22 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 23 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 24 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 25 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 26 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 27 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 28 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 29 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 30 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 31 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 32 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 33 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 34 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 35 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 36 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 37 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 38 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 39 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 40 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 41 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 42 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 43 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 44 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 45 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 46 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 47 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 48 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 49 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+        <tr className="tr2">
+          <td className="listings"> Track 50 </td>
+          <td className="listings"> 3:33 </td>
+          <td className="listings"> Jose </td>
+          <td className="listings"> Jose </td>
+        </tr>
+
       </table>
       </div>
     );
--- src/Sidenav.js
+++ src/Sidenav.js
@@ -2,36 +2,162 @@ import React, { Component } from 'react'
 // import ReactPlayer from 'react-player'
 import './Sidenav.css';
 class Sidenav extends Component {
+  constructor() {
+    super();
+ }
+ addRoom() {
+  alert("Add room was pressed");
+}
+
   render () {
     return (
       <div className="sidenav">
-        <table className="table1">
-          <tr>
-            <td className="td1"> Audio room </td>
-          </tr>
-          <tr>
-            <td className="td1"> Study Music room </td>
-          </tr>
-          <tr>
-            <td className="td1"> Alternative room </td>
-          </tr>
-          <tr>
-            <td className="td1"> Friends room </td>
-          </tr>
-          <tr>
-            <td className="td1"> Audio room </td>
-          </tr>
-          <tr>
-            <td className="td1"> Audio room </td>
-          </tr>
-        </table>
+        <div className="searchroom">
+          <i className="fas fa-plus-circle plus" id="plus" onClick={()=> this.addRoom()}></i>
+          <input className="inlink" type="text" name="name" />
+        </div>
+        <div className="sidescrollbox">
+          <table className="table1">
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Audio room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Study Music room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Alternative room </td>
+            </tr>
+            <tr>
+              <td className="td1"> Friends room </td>
+            </tr>
+          </table>
+        </div>
       </div>
     );
   }
-}
-
-function roomClick() {
-
 }
 
 export default Sidenav;
