ea390384f04e8c3e3f3624d9cf0473b8616ee65e
Josh <JTCheung3@gmail.com>
2018-07-05 14:50:19 -0700

    side nav addroom dropdown

--- src/Sidenav.js
+++ src/Sidenav.js
@@ -6,18 +6,49 @@ class Sidenav extends Component {
     super();
  }
  addRoom() {
-  alert("Add room was pressed");
-}
+  // alert("Add room was pressed");
+  var x = document.getElementById("addbox");
+   if (x.style.display == "none") {
+       x.style.display = "block";
+   } else {
+       x.style.display = "none";
+   }
+ }
+
+ searchRoom() {
+   var input = document.getElementById("room");
+   var filter = input.value.toUpperCase();
+   var table = document.getElementById("roomList");
+   var tr = table.getElementsByTagName("tr");
+   for (var i = 0; i < tr.length; i++) {
+      if(tr[i].innerHTML.toUpperCase().includes(filter)) {
+         tr[i].style.display = "";
+      }
+      else{
+        tr[i].style.display = "none";
+      }
+   }
+ }
 
   render () {
     return (
       <div className="sidenav">
         <div className="searchroom">
           <i className="fas fa-plus-circle plus" id="plus" onClick={()=> this.addRoom()}></i>
-          <input className="inlink" type="text" name="name" />
+          <input className="inlink" type="text" name="name" id="room" onChange={()=>this.searchRoom()}/>
+        </div>
+        <div  className="addbox" id="addbox">
+          <label className="linkT">
+            Room Name:
+          </label>
+          <input className="inlink"/>
+          <label className="linkT">
+            Room password:
+          </label>
+          <input className="inlink"/>
         </div>
         <div className="sidescrollbox">
-          <table className="table1">
+          <table className="table1" id="roomList">
             <tr>
               <td className="td1"> Audio room </td>
             </tr>
